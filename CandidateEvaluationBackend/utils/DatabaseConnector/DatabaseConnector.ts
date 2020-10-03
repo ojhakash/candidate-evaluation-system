2c497bdf09e0577dee9281f4ac70be3820edb3ed
akash <akash@emproto.com>
2020-10-03 23:33:42 +0530

    database config shited to env file

--- CandidateEvaluationBackend/utils/DatabaseConnector/DatabaseConnector.ts
+++ CandidateEvaluationBackend/utils/DatabaseConnector/DatabaseConnector.ts
@@ -1,17 +1,31 @@
 import { connect } from "http2";
 import { Sequelize, DataTypes, NOW } from "sequelize";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+process.env.database_name;
+process.env.database_user;
+process.env.database_password;
+process.env.database_host;
+process.env.database_dialect;
 export default class DatabaseConnector {
   private _sequelize: Sequelize;
 
   constructor() {
+    let database_name: any = process.env.database_name;
+    let database_user: any = process.env.database_user;
+    let database_password: any = process.env.database_password;
+    let database_host: any = process.env.database_host;
+    let database_dialect: any = process.env.database_dialect;
+
     this._sequelize = new Sequelize(
-      "candidate_evaluation_system",
-      "candidate_evaluation_admin",
-      "password",
+      database_name,
+      database_user,
+      database_password,
       {
-        host: "localhost",
-        dialect: "postgres",
+        host: database_host,
+        dialect: database_dialect,
         logging: false,
       }
     );
